f1e676a0cdaf11b330d715f0ac734591c3aa3f9a
curlybapchap <curlybapchap@hotmail.com>
2024-12-11 20:08:14 +0000

    refactor: add unit test for fixed rate rebate amount.

--- Smartwyre.DeveloperTest.Tests/PaymentService.Tests.cs
+++ Smartwyre.DeveloperTest.Tests/PaymentService.Tests.cs
@@ -23,4 +23,14 @@ public class PaymentServiceTests
         var result = new RebateService(rbt, prd, rbtReq).Calculate(rbtReq);
         Assert.True(result.Success == true, "Rebate service for product with FixedRateRebate returns true");
     }
+
+    [Fact]
+    public void FixedRateRebateCalculatorReturnsCorrectRebateAmount()
+    {
+        var rbtReq = new CalculateRebateRequest() { ProductIdentifier = "PrdA", RebateIdentifier = "Rbt1", Volume = 5, };
+        var rbt = new Rebate() { Identifier = "Rbt1", Incentive = IncentiveType.FixedRateRebate, Amount = 1, Percentage = 10, };
+        var prd = new Product() { Id = 1, Identifier = "PrdA", Price = 10.50m, SupportedIncentives = SupportedIncentiveType.FixedRateRebate, Uom = "kg" };
+        var result = new RebateService(rbt, prd, rbtReq).Calculate(rbtReq);
+        Assert.True(result.RebateAmount == 525, "Rebate service for product with FixedRateRebate returns true");
+    }
 }
